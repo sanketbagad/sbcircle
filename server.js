2f36d6ac1f19bd7ec4ed0200b58dc24fa414a9e8
Sanket Bagad <alencolins@gmail.com>
2021-08-14 16:45:24 +0530

    Almost Ready

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import auth from "./routes/api/auth.js"
 import users from "./routes/api/users.js"
 import posts from "./routes/api/posts.js"
 import profile from "./routes/api/profile.js"
+import path from "path"
 
 const app = express()
 
@@ -11,16 +12,22 @@ connectDB()
 
 app.use(express.json({ extended: false }))
 
-const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-    res.send("Fuck you")
-})
+
 
 app.use("/api/users", users) 
 app.use("/api/auth", auth) 
 app.use("/api/posts", posts) 
 app.use("/api/profile", profile) 
+
+if(process.env.NODE_ENV = "production") {
+    app.use(express.static("client/build"))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    })
+}
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server started at ${PORT}`)
